a0f30f1dbd9f933e913f17c2644a120867ae19ef
KMastroluca <132282028+KMastroluca@users.noreply.github.com>
2026-02-24 08:16:24 -0500

    Last Sync: 2026-02-24 08:16 (Mobile)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
-import { fromHono } from "chanfana";
-import { Hono } from "hono";
-// Start a Hono app
-const app = new Hono<{ Bindings: Env }>();
 
 
-app.get('/', (req, res) =>{
-    console.log("Hi");
-})
+export default {
+    
+    
+    async function fetch(req:Request, res:Response, ctx: any):Promise<Response> {
+        
+        console.log("Worker");
+        
+        
+        
+        
+    } satisfies ExportedHandler<Env>
+        
+    
+    
+    
+    
+    
+    
+}
 
